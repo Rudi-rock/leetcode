bb44ed7f7d8d79c7cde0fdd27ce7cb7a24ef335e
Rudra pratap <rudibaap@gmail.com>
2026-07-29 13:04:35 +0530

    Time: 754 ms (52.11%), Space: 19.7 MB (80.28%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.py
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.py
@@ -1,51 +1,69 @@
-from collections import Counter
-from math import comb
-
 class Solution:
-    def smallestPalindrome(self, s, k):
-        LIMIT = 10**6
+  def __init__(self):
+    self.MAX = 10**6 + 1
 
-        cnt = Counter(s)
+  def smallestPalindrome(self, s: str, k: int) -> str:
+    count = collections.Counter(s)
+    if not self._isPalindromePossible(count):
+      return ''
 
-        half = [0] * 26
-        mid = ""
+    halfCount, midLetter = self._getHalfCountAndMidLetter(count)
+    totalPerm = self._calculateTotalPermutations(halfCount)
+    if k > totalPerm:
+      return ''
+    leftHalf = self._generateLeftHalf(halfCount, k)
+    return ''.join(leftHalf) + midLetter + ''.join(reversed(leftHalf))
 
-        for c in sorted(cnt):
-            if cnt[c] % 2:
-                mid = c
-            half[ord(c) - ord("a")] = cnt[c] // 2
+  def _isPalindromePossible(self, count: collections.Counter) -> bool:
+    oddCount = sum(1 for count in count.values() if count % 2 == 1)
+    return oddCount <= 1
 
-        def ways(freq):
-            total = sum(freq)
-            ans = 1
-            rem = total
-            for x in freq:
-                if x:
-                    ans *= comb(rem, x)
-                    if ans > LIMIT:
-                        return LIMIT + 1
-                    rem -= x
-            return ans
+  def _getHalfCountAndMidLetter(self, count: collections.Counter) -> tuple[list[int], str]:
+    halfCount = [0] * 26
+    midLetter = ''
+    for c, freq in count.items():
+      halfCount[ord(c) - ord('a')] = freq // 2
+      if freq % 2 == 1:
+        midLetter = c
+    return halfCount, midLetter
 
-        if ways(half) < k:
-            return ""
+  def _calculateTotalPermutations(self, halfCount: list[int]) -> int:
+    """Calculate the total number of possible permutations."""
+    return self._countArrangements(halfCount)
 
-        left = []
+  def _generateLeftHalf(self, halfCount: list[int], k: int) -> list[str]:
+    """Generate the left half of the palindrome based on k."""
+    halfLen = sum(halfCount)
+    left = []
+    for _ in range(halfLen):
+      for i, freq in enumerate(halfCount):
+        if freq == 0:
+          continue
+        halfCount[i] -= 1
+        arrangements = self._countArrangements(halfCount)
+        if arrangements >= k:
+          left.append(chr(i + ord('a')))
+          break
+        else:
+          k -= arrangements
+          halfCount[i] += 1
+    return left
 
-        while sum(half):
-            for i in range(26):
-                if half[i] == 0:
-                    continue
+  def _countArrangements(self, count: list[int]) -> int:
+    """Calculate the number of possible arrangements of characters."""
+    total = sum(count)
+    res = 1
+    for freq in count:
+      res *= self._nCk(total, freq)
+      if res >= self.MAX:
+        return self.MAX
+      total -= freq
+    return res
 
-                half[i] -= 1
-                w = ways(half)
-
-                if w >= k:
-                    left.append(chr(i + ord("a")))
-                    break
-                else:
-                    k -= w
-                    half[i] += 1
-
-        left = "".join(left)
-        return left + mid + left[::-1]
+  def _nCk(self, n: int, k: int) -> int:
+    res = 1
+    for i in range(1, min(k, n - k) + 1):
+      res = res * (n - i + 1) // i
+      if res >= self.MAX:
+        return self.MAX
+    return res
